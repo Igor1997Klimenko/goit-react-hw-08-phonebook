04e810ff30c8c94057987b6ba9895b7c804e791d
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2022-06-21 18:31:52 +0300

    added code

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 const Footer = () => {
     return (
         <>
-        <div className={s.CallMe}>
+        <a href='tel:5554280940' className={s.CallMe}>
             <div className={s.CallmeMain}></div>
-        </div>
+        </a>
         <div className={s.Footer}>
                 <p className={s.FooterText} >&#169;opyright Igor Vladimirovich 2022</p>
                 <a className={s.Github} href="https://github.com/Igor1997Klimenko"><GitHubIcon/></a>
